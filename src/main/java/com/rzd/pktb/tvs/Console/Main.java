48de5ebe4234768dd65c29c5c921ab46fc038cc9
vasia pupkin <vtimoshenko@pktbcki.org.rzd>
2016-06-07 13:35:53 +0300

    initial commit

---
+++ src/main/java/com/rzd/pktb/tvs/Console/Main.java
@@ -0,0 +1,10 @@
+package com.rzd.pktb.tvs.Console;
+
+/**
+ * Created by vtimoshenko on 07.06.2016.
+ */
+public class Main {
+    public static void main (String[] args){
+        System.out.println("Hello world");
+    }
+}
