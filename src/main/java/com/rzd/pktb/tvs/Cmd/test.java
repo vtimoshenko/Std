c274d5c667fe2aace66e2a52863e2db2ad6f40ef
vtimoshenko <vtimoshenko@pktbcki.org.rzd>
2016-06-08 16:42:26 +0300

    Basic structure for console

--- src/main/java/com/rzd/pktb/tvs/Cmd/test.java
+++ src/main/java/com/rzd/pktb/tvs/Cmd/test.java
@@ -6,5 +6,11 @@ import com.rzd.pktb.tvs.Console.ConsoleCommand;
  * Created by vtimoshenko on 08.06.2016.
  */
 public class test implements ConsoleCommand{
-    public
+    public String Run (String[] args)
+    {
+        String rets = "";
+        if (args.length<2) rets = "No second argument!";
+        else rets = "You are pushing " + args[1];
+        return rets;
+    }
 }
--- src/main/java/com/rzd/pktb/tvs/Console/Main.java
+++ src/main/java/com/rzd/pktb/tvs/Console/Main.java
@@ -1,11 +1,14 @@
 package com.rzd.pktb.tvs.Console;
 
+import com.rzd.pktb.tvs.Cmd.test;
+
 /**
  * Created by vtimoshenko on 07.06.2016.
  */
 public class Main {
     public static void main (String[] args){
         ConsoleDialog dial = new ConsoleDialog();
+        dial.addCmd(new test());
         dial.run();
     }
 }
